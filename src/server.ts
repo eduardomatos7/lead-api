03c795500ac9b9c736f29e266bebe00548209d89
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-05-13 22:37:11 -0300

    feat: creates middleware and error files

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import cors from "cors"
 import router from "./routes/router"
+import { errorHandlerMiddleware } from "./middlewares/error-handler"
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use('/api', router)
-
+app.use(errorHandlerMiddleware)
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Servidor iniciado em http://localhost:${PORT}`))
