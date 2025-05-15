64f090dad5b46e6b9a80068650ea2b1cdd2320b6
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-05-15 12:34:22 -0300

    feat: create controller to show and create new lead

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -5,6 +5,9 @@ const router = Router()
 
 const leadsController = new LeadsController()
 router.get('/leads', leadsController.index)
+router.post('/leads', leadsController.create)
+router.get("/leads/:id", leadsController.show)
+
 router.get('/', async (req, res, next) => {
     try {
         res.json({ message: "Hello World" })
