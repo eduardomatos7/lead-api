83aa005b1b3b0807cdbb10ade9364ddf7228f742
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-05-14 23:47:14 -0300

    feat: create controller to manage leads

---
+++ src/controllers/LeadsController.ts
@@ -0,0 +1,13 @@
+import { Handler } from "express";
+import prisma from "../database";
+
+export class LeadsController {
+    index: Handler = async (req, res, next) => {
+        try {
+            const leads = await prisma.lead.findMany()
+            res.json(leads)
+        } catch (error) {
+            next(error)
+        }
+    }
+}
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,6 +1,10 @@
 import { Router } from "express"
+import { LeadsController } from "../controllers/LeadsController"
 
 const router = Router()
+
+const leadsController = new LeadsController()
+router.get('/leads', leadsController.index)
 router.get('/', async (req, res, next) => {
     try {
         res.json({ message: "Hello World" })
