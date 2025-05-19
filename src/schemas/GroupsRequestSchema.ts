e8a1ec3e599211bed4d865c646f31f5a4d0c2dc9
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-05-19 20:52:10 -0300

    feat: create schema

--- src/schemas/GroupsRequestSchema.ts
+++ src/schemas/GroupsRequestSchema.ts
@@ -9,3 +9,25 @@ export const UpdateGroupRequestSchema = z.object({
     name: z.string().optional(),
     description: z.string().optional()
 })
+
+const LeadStatusSchema = z.enum([
+    "New",
+    "Contacted",
+    "Qualified",
+    "Converted",
+    "Unresponsive",
+    "Disqualified",
+    "Archived",
+])
+export const GetGroupLeadsRequestSchema = z.object({
+    page: z.string().optional(),
+    pageSize: z.string().optional(),
+    name: z.string().optional(),
+    status: LeadStatusSchema.optional(),
+    sortBy: z.enum(["name", "createdAt"]).optional(),
+    order: z.enum(["asc", "desc"]).optional()
+})
+
+export const AddLeadsToGroupRequestSchema = z.object({
+    leadId: z.number()
+})
