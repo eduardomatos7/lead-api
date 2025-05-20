0d3d61ff1fa5e4a7c587cd6cc1f3361048c4b586
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-05-20 18:50:24 -0300

    feat(routes): update routes to use new controller instances

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,17 +1,7 @@
 import { Router } from "express"
-import { LeadsController } from "../controllers/LeadsController"
-import { GroupsController } from "../controllers/GroupsController"
-import { CampaignsController } from "../controllers/CampaignsController"
-import { CampaignLeadsController } from "../controllers/campaignLeadsController"
-import { GroupLeadsController } from "../controllers/GroupLeadsController"
+import { leadsController, groupsController, groupLeadsController, campaignsController, campaignLeadsController } from "../container"
 
 const router = Router()
-
-const leadsController = new LeadsController()
-const groupsController = new GroupsController()
-const campaignsController = new CampaignsController()
-const campaignLeadsController = new CampaignLeadsController()
-const groupLeadsController = new GroupLeadsController()
 
 router.get('/leads', leadsController.index)
 router.post('/leads', leadsController.create)
